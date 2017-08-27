ef6a29f3b2bd9a97aff2f2035768e82dfdcb97e8
vishalsanfran <vkhawarey@maprtech.com>
2017-08-27 14:34:38 -0700

    capture formColor input in callback

--- src/components_es6/AddColorForm.js
+++ src/components_es6/AddColorForm.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class AddColorForm extends Component {
+  constructor(props) {
+    super(props)
+    this.submit = this.submit.bind(this);
+  }
+  submit(e) {
+    const {_title, _color} = this.refs;
+    this.props.onNewColor(_title.value, _color.value);
+    _title.value = '';
+    _color.value = '#000000';
+    _title.focus();
+  }
   render() {
     return (
-      <form onSubmit={e=>e.preventDefault()}>
-        <input type="text" placeholder="color title" required />
-        <input type="color" required />
+      <form onSubmit={this.submit}>
+        <input ref="_title" type="text" placeholder="color title" required />
+        <input ref="_color" type="color" required />
         <button>Add Color</button>
       </form>
     )
   }
+}
+
+AddColorForm.propTypes = {
+  onNewColor: PropTypes.func
+}
+
+AddColorForm.defaulProps = {
+  onNewColor: f=>f
 }
 
 export default AddColorForm;
--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import data from './data/static_recipes';
 import AddColorForm from './components_es6/AddColorForm';
 
 window.React = React;
+const logColor = (title, color) =>
+  console.log(`New color: ${title}| ${color}`)
 ReactDOM.render(
-  <AddColorForm />,
+  <AddColorForm onNewColor={logColor} />,
   document.getElementById('colorpicker'));
 ReactDOM.render(
   <Menu recipes={data} title="Delicious Recipes" />,
