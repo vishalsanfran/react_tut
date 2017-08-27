7f593f133e155ae9d2d402f0009b398ff2412f6b
vishalsanfran <vkhawarey@maprtech.com>
2017-08-27 07:11:53 -0700

    refactor instructions.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Instructions from './Instructions';
 
 const Recipe = ({ name, ingredients, steps }) =>
     <section id={name.toLowerCase().replace(/ /g, "-")}>
@@ -8,12 +9,7 @@ const Recipe = ({ name, ingredients, steps }) =>
                 <li key={i}>{ingredient.name}</li>
             )}
         </ul>
-        <section className="instructions">
-            <h2>Cooking Instructions</h2>
-            {steps.map((step, i) =>
-                <p key={i}>{step}</p>
-            )}
-        </section>
+        <Instructions title="Cooking instructions" steps={steps} />
    </section>
 
 const Menu = ({ title, recipes }) =>
---
+++ src/Instructions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Instructions = ({title, steps}) =>
+<section className="instructions">
+    <h2>{title}</h2>
+    {steps.map((step, i) =>
+        <p key={i}>{step}</p>
+    )}
+</section>
+
+export default Instructions
