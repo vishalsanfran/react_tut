b073311c53efcc1db06e0454ad24a26d542f5cdc
vishalsanfran <vkhawarey@maprtech.com>
2017-08-27 11:03:40 -0700

    custom validator for ingredient name

--- src/components_es6/Ingredient.js
+++ src/components_es6/Ingredient.js
@@ -17,7 +17,14 @@ class Ingredient extends React.Component {
 Ingredient.propTypes = {
   amount: PropTypes.number,
   measurement: PropTypes.string,
-  name: PropTypes.string.isRequired
+  name: (props, propName) =>
+    (typeof props[propName] !== 'string' ?
+        new Error("Name must be string") :
+        (props[propName].length < 1) ?
+            new Error("Length is too small"):
+            (props[propName].length > 20) ?
+                new Error("Length is over 20 chars"):
+                null)
 }
 
 export default Ingredient;
