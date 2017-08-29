5cefaad9eabea5a6dd5e3051b6068a23cce6d54a
vishalsanfran <vkhawarey@maprtech.com>
2017-08-29 15:34:37 -0700

    adding state, ratings dont move

---
+++ src/components/Star.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Star = ({ selected=false, onClick=f=>f }) =>
+  <div className={(selected) ? "star selected" : "star"}>
+  </div>
+Star.propTypes = {
+  selected: PropTypes.bool,
+  onClick: PropTypes.func
+}
+
+export default Star;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import Menu from './components/Menu';
 import registerServiceWorker from './registerServiceWorker';
 import data from './data/static_recipes';
 import AddColorForm from './components_es6/AddColorForm';
+import StarRating from './components_es6/StarRating'
 
 window.React = React;
 const logColor = (title, color) =>
   console.log(`New color: ${title}| ${color}`)
+ReactDOM.render(
+  <StarRating totalStars={6} starsSelected={3} />,
+  document.getElementById('star-rating'));
 ReactDOM.render(
   <AddColorForm onNewColor={logColor} />,
   document.getElementById('colorpicker'));
