1cceb530764819b0a2d2307c2c5ba47b73fefb18
vishalsanfran <vkhawarey@maprtech.com>
2017-09-08 21:54:13 -0700

    comp creation lifecycle with promise

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 //import data from './data/static_recipes';
 //import AddColorForm from './components_es6/AddColorForm';
 //import StarRating from './components_es6/StarRating'
-import App from './App'
+import MemberList from './components_es6/MemberList'
+//import App from './App'
 
 window.React = React;
 /*const logColor = (title, color) =>
@@ -21,8 +22,12 @@ ReactDOM.render(
 ReactDOM.render(
   <Menu recipes={data} title="Delicious Recipes" />,
   document.getElementById('root'));
-*/
+
 ReactDOM.render(
   <App colors={[]} />,
+  document.getElementById('root'));
+*/
+ReactDOM.render(
+  <MemberList count={3} />,
   document.getElementById('root'));
 registerServiceWorker();
